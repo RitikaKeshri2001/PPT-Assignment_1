a01d501ca228ce91c144f2a09944a569977d127d
RitikaKeshri2001 <124250380+RitikaKeshri2001@users.noreply.github.com>
2023-06-23 17:49:34 +0530

    Update Q3_Search_Insert.java

--- Q3_SearchElement.java
+++ Q3_SearchElement.java
@@ -1,21 +1,24 @@
 public class Q3_SearchElement {
 
-    public static int valIndex(int[] nums, int target){
-        int i = nums.length/2;
-        while(i>=0 && i<nums.length){
-            if(nums[i] == target) return i;
-            else if(nums[i] < target){
-                i++;
+    public static int searchInsert(int[] nums, int target){
+        int low = 0;
+        int high = nums.length-1;
+        while(low<=high) {
+            int mid = low + (high-low)/2;
+            if(nums[mid] == target) {
+                return mid;
+            } else if(nums[mid] > target) {
+                high = mid - 1;
             } else {
-                i--;
+                low = mid+1;
             }
         }
-        return -1;
+        return low;
     }
 
     public static void main(String[] args) {
         int nums[] = {1,3,5,6};
         int target = 5;
-        System.out.println(valIndex(nums, target));
+        System.out.println(searchInsert(nums, target));
     }
 }
